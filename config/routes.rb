4ecebdbe3de90b7fe6fa53125daa5ecf0404b497
Dominik Mackovic <dominik.mackovic@protonmail.com>
2024-12-13 23:24:23 +0100

    Add line item dates model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :quotes
+
+  resourcces :quotes do
+    resources :line_item_dates, expect: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
