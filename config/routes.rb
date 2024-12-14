d91f3802e39f7ce480a605efb9eda71959a4c625
Dominik Mackovic <dominik.mackovic@protonmail.com>
2024-12-15 00:08:13 +0100

    Added update, edit and delete actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :quotes
 
-  resourcces :quotes do
+  resources :quotes do
     resources :line_item_dates, expect: [:index, :show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
